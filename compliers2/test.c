68190ab783f5a5a1837b6d3ea17cf661836b2439
xdober <xuduo1017@live.com>
2016-12-07 20:08:02 +0800

    finiah semantics

--- compliers2/test.c
+++ compliers2/test.c
@@ -1,7 +1,7 @@
-struct curcle {
+struct circle {
 	struct point {
 		int x;
-		int y;
+		float y;
 	};
 	int rad;
 };
@@ -17,6 +17,9 @@ int main(){
 	num1 = 3;
 	nums[1] = 4;
 	if(num1 == 1) {
-	flos[2] = flo1 + flos[1];
+		flos[2] = flo1 + flos[1];
+		if(num1 == 1) {
+			num1 = 4;
+		}
 	}
 }
